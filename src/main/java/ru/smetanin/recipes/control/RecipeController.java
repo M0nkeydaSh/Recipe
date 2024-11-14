7d8b946c905d09f83fc73ec443c1231ba718fda3
Senpai <senpai@mail.ru>
2024-11-14 23:41:19 +0300

    TASK3246: Refactoring get recipe method

--- src/main/java/ru/smetanin/recipes/control/RecipeController.java
+++ src/main/java/ru/smetanin/recipes/control/RecipeController.java
@@ -2,6 +2,7 @@ package ru.smetanin.recipes.control;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.smetanin.recipes.dto.RecipeDto;
 import ru.smetanin.recipes.service.RecipeService;
@@ -10,33 +11,35 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping ("/api/v1/recipes")
+@RequestMapping("/api/v1/recipes")
 @RequiredArgsConstructor
 public class RecipeController {
 
-    private final RecipeService  recipeService;
-    @GetMapping ("/getAll")
-    public List<RecipeDto> getAll(){
+    private final RecipeService recipeService;
+
+    @GetMapping("/getAll")
+    public List<RecipeDto> getAll() {
         return recipeService.getAll();
     }
 
     @DeleteMapping("/delete/{id}")
-    public HttpStatus delete(@PathVariable String id){
+    public HttpStatus delete(@PathVariable String id) {
         recipeService.delete(id);
         return HttpStatus.OK;
     }
 
     @PostMapping("/add")
-    public RecipeDto add(@RequestBody RecipeDto recipeDto){
+    public RecipeDto add(@RequestBody RecipeDto recipeDto) {
         return recipeService.add(recipeDto);
     }
 
-    @GetMapping ("/getOne/{id}")
-    public RecipeDto getOne(String id){
-        return recipeService.getOne(id);
+    @GetMapping("/get/{id}")
+    public ResponseEntity<RecipeDto> getOne(@PathVariable String id) {
+        return new ResponseEntity<>(recipeService.getOne(id), HttpStatus.OK);
     }
-    @PutMapping ("/update")
-    public RecipeDto update(@RequestBody RecipeDto recipeDto){
+
+    @PutMapping("/update")
+    public RecipeDto update(@RequestBody RecipeDto recipeDto) {
         return recipeService.update(recipeDto);
     }
 
